9676edab3f386a137182353f191049eafa87f26f
Alex Coupe <acoupe@gmail.com>
2019-04-15 16:13:46 +0100

    intial commit of calculator

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import KeypadComponent from './Components/keypad-component';
+import ResultComponent from './Components/result-component';
+import './App.css';
+
+class App extends Component {
+  
+  constructor(){
+    super();
+
+    this.state = {
+      result:""
+    }
+  }
+
+  reset = () => {
+    this.setState({
+      result:""
+    })
+  };
+
+  backspace = () => {
+    this.setState({
+      result: this.state.result.slice(0,-1)
+    })
+  };
+
+  calculate = () => {
+    try {
+      this.setState({
+         // eslint-disable-next-line
+        result: (eval(this.state.result) || "") + ""
+      })
+    } catch (e)
+    {
+      this.setState.result({
+        result: "error"
+      })
+    }
+  };
+
+  onClick = button => {
+
+    if (button === "=")
+    {
+      this.calculate();
+    }
+
+    else if (button === "C")
+    {
+      this.reset();
+    }
+
+    else if (button === "CE")
+    {
+      this.backspace();
+    }
+
+    else {
+      this.setState({
+        result: this.state.result + button
+      })
+    }
+
+  };
+  
+  render() {
+    return (
+      <div className="calculator-body">
+      <h1>Calculator</h1>
+      <ResultComponent result = {this.state.result}/>
+      <KeypadComponent onClick = {this.onClick}/>
+       
+      </div>
+    );
+  }
+}
+
+export default App;
