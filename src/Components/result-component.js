7e19000ba0ed19512c2eeed85e93f4569a263543
Alex Coupe <acoupe@gmail.com>
2019-04-16 11:19:22 +0100

    styled the result component.

--- src/Components/result-component.js
+++ src/Components/result-component.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
+import "./result-component.css";
 
 class ResultComponent extends Component {
 //Displays the result that it passed from the parent 'App' component
     render() {
         let {result} = this.props;
+        if (result === '')
+        {
+            result = 0;
+        }
         return ( <div className="result">
                  <p>{result}</p>
                  </div>);
